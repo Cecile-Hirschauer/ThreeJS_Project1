ed7fbef87c9e01229bc4da3e81d6a3c039bcceb8
cecile hirschauer <cecile.h.75@gmail.com>
2023-09-14 18:05:28 +0200

    Create cube, plan, sphere add light

---
+++ README.md
@@ -0,0 +1,22 @@
+## Three.js Project
+This is a basic project to demonstrate various 3D geometries and functionalities using Three.js. The project includes a rotating cube, a moving sphere, and several lighting conditions, along with grid and axis helpers.
+
+### Features
+* Rotating cube
+* Moving sphere
+* Plane with shadow reception
+* Directional light with shadow casting
+* Spotlight
+* Grid helper
+* Axes helper
+* GUI controls to change properties dynamically
+
+### Built With
+* Three.js - JavaScript 3D library
+* dat.GUI - GUI for data manipulation
+* Parcel - Web application bundler
+
+### Running the Application
+Run the development server using Parcel:
+```parcel ./src/index.html```
+Navigate to http://localhost:1234 (or the port shown in your terminal) to see your 3D scene in action.
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,42 +1,119 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import dat from "dat.gui/src/dat";
 
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize( window.innerWidth, window.innerHeight );
+// enable shadows
+renderer.shadowMap.enabled = true;
 
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
+// Camera positioning
 const camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
 
-const orbit = new OrbitControls( camera, renderer.domElement );
-
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+// Sets orbit control to move the camera around
+const orbit = new OrbitControls(camera, renderer.domElement);
 
 // camera.position.z = 5;
 // camera.position.y = 2;
-camera.position.set( 0, 2, 5 );
+camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const box = new THREE.Mesh( boxGeometry, boxMaterial );
-scene.add( box );
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+// Create a plane that receives shadows (but does not cast them)
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide
+});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
+
+// add grid helper
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+// Sets the x, y, and z axes with each having a length of 4
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// create a sphere
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    color: 0x0000ff,
+    wireframe: false
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+sphere.position.set(-10, 10, 0)
+sphere.castShadow = true;
+
+// create an ambient light
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionLight = new THREE.DirectionalLight(0xffffff, 0.8);
+scene.add(directionLight);
+directionLight.position.set(-30, 50, 0);
+directionLight.castShadow = true;
+directionLight.shadow.camera.bottom = -12;
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionLight, 5);
+scene.add(directionalLightHelper);
+
+const directionalLightShadowsHelper = new THREE.CameraHelper(directionLight.shadow.camera);
+scene.add(directionalLightShadowsHelper);
+
+// create a spotlight
+const spotlight = new THREE.SpotLight(0xffffff, 0.8);
+scene.add(spotlight);
+spotlight.position.set(-100, 100, 0);
+const spotlightHelper = new THREE.SpotLightHelper(spotlight);
+scene.add(spotlightHelper);
+
+// data entry with dat.gui
+const gui = new dat.GUI();
+const options = {
+    sphereColor: '#ffea00',
+    wireframe: false,
+    speed: 0.01,
+};
+gui.addColor(options, 'sphereColor').onChange((e) => {
+    sphereMaterial.color.set(e);
+})
+
+gui.add(options, 'wireframe').onChange((e) => {
+    sphere.material.wireframe = e;
+});
+gui.add(options, 'speed', 0, 0.1);
+let stop = 0;
 
 const animate = function (time) {
     box.rotation.x = time / 2000
     box.rotation.y = time / 1000;
-    renderer.render( scene, camera );
+
+    stop += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(stop));
+
+    renderer.render(scene, camera);
 };
 
-renderer.setAnimationLoop( animate );
+renderer.setAnimationLoop(animate);
 
-renderer.render( scene, camera );
+renderer.render(scene, camera);
